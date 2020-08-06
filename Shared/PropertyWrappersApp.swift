1e9ec1540b797b3bd863988dd569baf81d1341b1
FranicevicNikola <nikola007f@gmail.com>
2020-08-06 16:05:16 +0200

    Initial Commit

---
+++ Shared/PropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersApp.swift
+//  Shared
+//
+//  Created by Master Family on 06/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
