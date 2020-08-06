33794091503593c7171f7e43f438543d799f4f68
FranicevicNikola <nikola007f@gmail.com>
2020-08-06 17:28:17 +0200

    finished binding

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var intValue = 0
     var body: some View {
-        Text("Hello, world!").padding()
+        StateExample()
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+
+struct StateExample: View {
+    @State private var intValue = 0
+    
+    var body: some View {
+        VStack {
+            Text("intValue equals \(intValue)")
+            
+            BindingView(intValue: $intValue)
+            
+        }
     }
 }
+
+// You should use @State if:
+// The view itself creates(and owns) the instance you want to wrap.
+// You need to respond to changes that occur within the wrapped property
+// You're wrapping a value type (struct or enum)
+
+
+
+
+struct BindingView: View {
+    @Binding var intValue: Int
+    
+    var body: some View {
+        VStack {
+            Button("increment") {
+                intValue += 1
+            }
+        }
+            
+    }
+}
+
+//You should use @Binding if :
+//You need read adn write access to a aproperty that's owned by a parent view
+//The wrapped property is a value type (struct or enum)
+//You don't own the wrapped property(it's provided by a parent view)
